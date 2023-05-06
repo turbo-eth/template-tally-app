7f5885b7b0ebaf3c4a2996b8cbd1e84522c0c023
Vitor <marthendalnunes@gmail.com>
2023-05-05 22:11:50 -0300

    feat: add tally client updates

---
+++ integrations/tally/config/chains.ts
@@ -0,0 +1,73 @@
+export const tallyChains = [
+  {
+    id: undefined,
+    name: 'All',
+  },
+  {
+    id: 'eip155:1',
+    name: 'Ethereum',
+    logoSrc: '/icons/chains/ethereum.svg',
+  },
+  {
+    id: 'eip155:137',
+    name: 'Polygon',
+    logoSrc: '/icons/chains/polygon.svg',
+  },
+  {
+    id: 'eip155:10',
+    name: 'Optimism',
+    logoSrc: '/icons/chains/optimism.svg',
+  },
+  {
+    id: 'eip155:43114',
+    name: 'Avalanche',
+    logoSrc: '/icons/chains/avalanche.svg',
+  },
+
+  {
+    id: 'eip155:56',
+    name: 'BNB Chain',
+    logoSrc: '/icons/chains/bnb.svg',
+  },
+  {
+    id: 'eip155:42161',
+    name: 'Arbitrum',
+    logoSrc: '/icons/chains/arbitrum.svg',
+  },
+  {
+    id: 'eip155:100',
+    name: 'Gnosis',
+    logoSrc: '/icons/chains/gnosis.svg',
+  },
+]
+
+export const tallyChainObjects = {
+  'eip155:1': {
+    name: 'Ethereum',
+    logoSrc: '/icons/chains/ethereum.svg',
+  },
+  'eip155:137': {
+    name: 'Polygon',
+    logoSrc: '/icons/chains/polygon.svg',
+  },
+  'eip155:10': {
+    name: 'Optimism',
+    logoSrc: '/icons/chains/optimism.svg',
+  },
+  'eip155:43114': {
+    name: 'Avalanche',
+    logoSrc: '/icons/chains/avalanche.svg',
+  },
+  'eip155:56': {
+    name: 'BNB Chain',
+    logoSrc: '/icons/chains/bnb.svg',
+  },
+  'eip155:42161': {
+    name: 'Arbitrum',
+    logoSrc: '/icons/chains/arbitrum.svg',
+  },
+  'eip155:100': {
+    name: 'Gnosis',
+    logoSrc: '/icons/chains/gnosis.svg',
+  },
+}
